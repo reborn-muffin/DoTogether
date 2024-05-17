a4ec757c4889321bd606203a21652545542090bd
bohdanda <reborn.muffin@gmail.com>
2024-05-17 17:18:32 +0300

    refactoring

--- src/components/ThemeSegment.tsx
+++ src/components/ThemeSegment.tsx
@@ -1,8 +1,8 @@
-import {Appearance, View} from 'react-native'
+import {Appearance, ColorSchemeName, View} from 'react-native'
 import SettingsSegment from './ui/SettingsSegment'
-import setColorScheme = Appearance.setColorScheme
 import {Text} from 'react-native-paper'
 import * as SecureStore from 'expo-secure-store'
+import setColorScheme = Appearance.setColorScheme
 
 const ThemeSegment = () => {
   const prevValue = SecureStore.getItem('colorScheme')
@@ -13,8 +13,7 @@ const ThemeSegment = () => {
   ]
 
   const handleValueChange = (value: string) => {
-    const colorScheme: null | string = value === '' ? null : value
-    setColorScheme(colorScheme)
+    setColorScheme(value === '' ? (value as ColorSchemeName) : null)
     SecureStore.setItem('colorScheme', value)
   }
 
